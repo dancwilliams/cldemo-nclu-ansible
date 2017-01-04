69a43996a84d3f36c90965c4e12c015c71be0e2a
Barry Peddycord III <ishara@isharacomix.org>
2017-01-04 16:05:01 -0500

    hmmm i made an oversight oops

--- library/nclu.py
+++ library/nclu.py
@@ -102,8 +102,7 @@ def command_helper(module, command, errmsg=None):
 
 def check_pending(module):
     """Check the pending diff of the nclu buffer."""
-    pending = command_helper(module, "pending", "check pending failed")
-    #TODO: when check pending fails, errors are present in potential config
+    pending = command_helper(module, "pending", "Error in pending config. You may want to view `net pending` on this target.")
 
     delimeter1 = "net add/del commands since the last 'net commit'"
     color1 = '\x1b[94m'
